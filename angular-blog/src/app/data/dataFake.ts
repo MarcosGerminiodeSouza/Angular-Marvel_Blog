ce1ed9ce7cc812a4b20c2a4ba29d40fc8e1ef86b
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-01-31 20:55:06 -0300

    create dataFake

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+{
+  "id": 1,
+  "photo": "url",
+  "title": "TITULO",
+  "description": "descrição do título"
+},
+{
+  "id": 2,
+  "photo": "url",
+  "title": "TÍTULO",
+  "description": "descrição do título"
+}
+]
