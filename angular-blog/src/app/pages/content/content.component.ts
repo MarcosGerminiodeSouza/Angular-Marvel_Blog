691cd3c02fb98353b3095bf121e5badbd49fb8bb
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-01-31 20:56:15 -0300

    create component content-page

--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-
+  photoCover:string = "https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/fccc6398-0f8f-11ed-aa6e-9587410378a2/tony-stark-snap.jpg"
+  contentTitle:string = "MINHAS ULTIMAS PALAVRAS"
+  contentDescription:string = "EU SOU O HOMEM DE FERRO!!!"
   constructor() { }
 
   ngOnInit(): void {
