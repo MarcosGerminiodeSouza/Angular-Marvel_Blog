2588872fb33368dce16110a21b7325ca2932c550
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-02-01 18:41:10 -0300

    edit dataFake

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -1,26 +1,34 @@
 export const dataFake = [
-{
-  "id": "1",
-  "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg",
-  "title": "NOVO HOMEM DE FERRO EM 3D",
-  "description": "Marvel Studios anuncia novo filme do Homem de Ferro confira!"
-},
-{
-  "id": "2",
-  "photoCover": "https://cdn1.epicgames.com/offer/ea73c08ac633414584e281f838b3a78c/EGS_MarvelsMidnightSuns_FiraxisGames_S1_2560x1440-2f3d597f3afd9e9fd4cea1df3394705e",
-  "title": "JOGOS | MARVEL'S MIDNIGHT SUNS GANHA PERSONAGENS E DATA DE LANÇAMENTO NA EPIC GAMES",
-  "description": "JOGOS | MARVEL'S MIDNIGHT SUNS GANHA PERSONAGENS E DATA DE LANÇAMENTO NA EPIC GAMES"
-},
-{
-  "id": "3",
-  "photoCover": "https://cinebuzz.uol.com.br/media/uploads/echo-marvel.jpg",
-  "title": "SÉRIES - 2023 | ECHO: NOVA SÉRIE DA MARVEL ANUNCIA NO DISNEY +",
-  "description": "SÉRIES - 2023 | ECHO: NOVA SÉRIE DA MARVEL ANUNCIA NO DISNEY +"
-},
-{
-  "id": "4",
-  "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/01/homem_formiga_3_capa__0o0128kem-1210x544.jpg",
-  "title": "FILMES - 2023 | HOMEM-FORMIGA E A VESPA: QUANTUMANIA",
-  "description": "FILMES - 2023 | HOMEM-FORMIGA E A VESPA: QUANTUMANIA"
-}
+  {
+    "id": "1",
+    "title": "THANOS É DERROTADO EM VINGADORES",
+    "description": "Tony Stark estalou os dedos com a manopla do Infinito e salvou o mundo das garras de Thanos. Vingadores então recebe um final emocionante com Thanos sendo derrotado desaparecendo. Nenhuma frase seria mais impactante quando Tony Stark dizendo: Eu sou o HOMEM DE FERRO!",
+    "photoCover": "https://www.fatosdesconhecidos.com.br/wp-content/uploads/2019/04/thanos-fatos-nerd.jpg"
+  },
+  {
+    "id": "2",
+    "title": "MARVEL'S MIDNIGHT SUNS GANHA PERSONAGENS E DATA DE LANÇAMENTO",
+    "description": "Jogue agora mesmo MARVEL'S MIDNIGHT SUNS na Epic Games",
+    "photoCover": "https://cdn1.epicgames.com/offer/ea73c08ac633414584e281f838b3a78c/EGS_MarvelsMidnightSuns_FiraxisGames_S1_2560x1440-2f3d597f3afd9e9fd4cea1df3394705e"
+  },
+  {
+    "id": "3",
+    "title": "ECHO: NOVA SÉRIE DA MARVEL",
+    "description": "Assista agora mesmo Echo: a nova série da Marvel na DISNEY +",
+    "photoCover": "https://as01.epimg.net/meristation/imagenes/2022/05/17/noticias/1652806537_998017_1652806789_noticia_normal.jpg"
+  },
+  {
+    "id": "4",
+    "photoCover": "http://disneyplusbrasil.com.br/wp-content/uploads/2022/07/Homem-Formiga-3.jpg",
+    "title": "HOMEM-FORMIGA E A VESPA: QUANTUMANIA",
+    "description": "HOMEM-FORMIGA E A VESPA: QUANTUMANIA. Hoje nos cinamas"
+  },
+
+  {
+    "id": "5",
+    "title": 'Marvel anuncia "All-Out Avengers", nova HQ dos Vingadores',
+    "description": "O quadrinho irá revelar informações não só sobre os vingadores pré-históricos mas também sobre a própria fundação do Universo Marvel. Além disso, a revista também irá explorar detalhes acerca da misteriosa conexão entre Thor e a Força Fênix.",
+    "photoCover":"https://pop.proddigital.com.br/wp-content/uploads/sites/8/2022/04/nova-hq-dos-vingadores-e1649705407518.jpg"
+  }
+
 ]
