b802bf24bd63222f09abfb8c556653329b13e5f1
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-02-01 10:15:49 -0300

    add mock visual to big-card

--- angular-blog/src/app/components/big-card/big-card.component.ts
+++ angular-blog/src/app/components/big-card/big-card.component.ts
@@ -7,11 +7,11 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BigCardComponent implements OnInit {
   @Input()
-  photoCover:string = ""
+  photoCover:string = "https://complianz.io/wp-content/uploads/2019/03/placeholder-300x202.jpg"
   @Input()
-  cardTitle:string = ""
+  cardTitle:string = "DÊ UM TITULO AO SEU BIG-CARD"
   @Input()
-  cardDescription:string = ""
+  cardDescription:string = "Dê uma descrição ao seu BIG-CARD"
   @Input()
   Id:string = "0"
 
