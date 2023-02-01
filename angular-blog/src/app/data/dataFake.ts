94ebeb7e6ba360da64d8c8195b7dbc6cd5fb1a35
MarcosGerminiodeSouza <marcos.germiniodesouza@gmail.com>
2023-01-31 22:37:47 -0300

    erro no data-fake

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
 {
-  "id": 1,
-  "photo": "url",
-  "title": "TITULO",
-  "description": "descrição do título"
+  "id": "1",
+  "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/AD72115B9CDAE873B2234BD0908CEF7B45A2D9AD9C66C699239B46E582F70A8B/scale?width=1200&aspectRatio=1.78&format=jpeg",
+  "title": "NOVO HOMEM DE FERRO EM 3D",
+  "description": "Marvel Studios anuncia novo filme do Homem de Ferro confira!"
 },
 {
-  "id": 2,
-  "photo": "url",
-  "title": "TÍTULO",
-  "description": "descrição do título"
+  "id": "2",
+  "photoCover": "https://cdn1.epicgames.com/offer/ea73c08ac633414584e281f838b3a78c/EGS_MarvelsMidnightSuns_FiraxisGames_S1_2560x1440-2f3d597f3afd9e9fd4cea1df3394705e",
+  "title": "JOGOS | MARVEL'S MIDNIGHT SUNS GANHA PERSONAGENS E DATA DE LANÇAMENTO NA EPIC GAMES",
+  "description": "JOGOS | MARVEL'S MIDNIGHT SUNS GANHA PERSONAGENS E DATA DE LANÇAMENTO NA EPIC GAMES"
+},
+{
+  "id": "3",
+  "photoCover": "https://cinebuzz.uol.com.br/media/uploads/echo-marvel.jpg",
+  "title": "SÉRIES - 2023 | ECHO: NOVA SÉRIE DA MARVEL ANUNCIA NO DISNEY +",
+  "description": "SÉRIES - 2023 | ECHO: NOVA SÉRIE DA MARVEL ANUNCIA NO DISNEY +"
+},
+{
+  "id": "4",
+  "photoCover": "https://uploads.jovemnerd.com.br/wp-content/uploads/2023/01/homem_formiga_3_capa__0o0128kem-1210x544.jpg",
+  "title": "FILMES - 2023 | HOMEM-FORMIGA E A VESPA: QUANTUMANIA",
+  "description": "FILMES - 2023 | HOMEM-FORMIGA E A VESPA: QUANTUMANIA"
 }
 ]
--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -7,11 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
   photoCover:string = "https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/fccc6398-0f8f-11ed-aa6e-9587410378a2/tony-stark-snap.jpg"
-  contentTitle:string = "MINHAS ULTIMAS PALAVRAS"
+  contentTitle:string = "MINHAS ULTIMAS PALAVRAS?"
   contentDescription:string = "EU SOU O HOMEM DE FERRO!!!"
-  constructor() { }
+  private id:string  | null = "0"
+
+  constructor(private route:ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+  }
+
+  setValuesToComponent(id:string | null) {
+    const result = dataFake.filter(article => article.id == id)[0]
+    this.photoCover = result.photoCover
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+
+
   }
 
 }
